2edb58f169da5af415ad8dbdb6048a99c179f4bb
lin231135 <lin231135@uvg.edu.gt>
2026-03-23 22:14:19 -0600

    feat: extender modelos de datos con soporte para acciones YALex

--- modelos/nodo.py
+++ modelos/nodo.py
@@ -1,8 +1,9 @@
 class Nodo:
     """Representa un nodo dentro del árbol sintáctico de la expresión regular."""
-    def __init__(self, valor, id_posicion=None):
+    def __init__(self, valor, id_posicion=None, accion=None):
         self.valor = valor
         self.id_posicion = id_posicion # Solo las hojas (operandos) tienen ID
+        self.accion = accion           # Guarda la acción de YALex
         self.hijo_izq = None
         self.hijo_der = None
         
